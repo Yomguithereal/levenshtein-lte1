a6125e932408b68ba406459323d6c8ac805145da
Yomguithereal <guillaumeplique@gmail.com>
2019-12-19 16:41:21 +0100

    levenshteinLte1

--- index.js
+++ index.js
@@ -1,7 +1,76 @@
 /**
- * levenshtein-lte1
- * =================
+ * Function returning Levenshtein distance between two strings if it is less
+ * than or equal to 1, else returning Infinity.
  *
- * Library endpoint.
+ * @param {string|Array} A - First sequence.
+ * @param {string|Array} B - Second sequence.
+ * @param {number}         - Distance.
  */
+function levenshteinLte1(A, B) {
 
+  // Are both strings the same?
+  // TODO: bench
+  if (A === B)
+    return 0;
+
+  var tmp;
+
+  // A should be the shortest string
+  if (A.length > B.length) {
+    tmp = A;
+    A = B;
+    B = tmp;
+  }
+
+  var la = A.length,
+      lb = B.length;
+
+  // Early termination
+  // TODO: can factorize with delta?
+  if (la === 0)
+    return lb === 1 ? 1 : Infinity;
+
+  var delta = lb - la;
+
+  if (delta > 1)
+    return Infinity;
+
+  var cost = 0,
+      i = 0,
+      j = 0;
+
+  // Addition
+  if (delta === 1) {
+    while (i < la) {
+      if (A[i] === B[j]) {
+        j++;
+        i++;
+        continue;
+      }
+
+      if (cost === 1)
+        return Infinity;
+
+      cost = 1;
+      j++;
+    }
+
+    return 1;
+  }
+
+  // Substitution
+  while (i < la) {
+    if (A[i] !== B[i]) {
+      if (cost === 1)
+        return Infinity;
+
+      cost = 1;
+    }
+
+    i++;
+  }
+
+  return cost;
+}
+
+module.exports = levenshteinLte1;
--- test.js
+++ test.js
@@ -3,3 +3,116 @@
  * ============================
  */
 var assert = require('assert');
+var levenshteinLte1 = require('./index.js');
+
+var BASIC_TESTS = [
+  [['b', 'o', 'o', 'k'], ['b', 'a', 'c', 'k'], 2],
+  [['the', 'cat', 'eats', 'mouse'], ['the', 'mouse', 'likes', 'mouse'], 2],
+  ['book', 'back', 2],
+  ['bbbbookkkk', 'bbbbackkkk', 2],
+  ['hello', 'helo', 1],
+  ['good sir', 'baal', 8],
+  ['say', 'shiver', 5],
+  ['feature', 'get-project-features', 13],
+  ['example', 'samples', 3],
+  ['sturgeon', 'urgently', 6],
+  ['levenshtein', 'frankenstein', 6],
+  ['distance', 'difference', 5],
+  ['a', 'b', 1],
+  ['ab', 'ac', 1],
+  ['ac', 'bc', 1],
+  ['abc', 'axc', 1],
+  ['xabxcdxxefxgx', '1ab2cd34ef5g6', 6],
+  ['a', '', 1],
+  ['ab', 'a', 1],
+  ['ab', 'b', 1],
+  ['abc', 'ac', 1],
+  ['xabxcdxxefxgx', 'abcdefg', 6],
+  ['', 'a', 1],
+  ['a', 'ab', 1],
+  ['b', 'ab', 1],
+  ['ac', 'abc', 1],
+  ['abcdefg', 'xabxcdxxefxgx', 6],
+  ['', '', 0],
+  ['a', 'a', 0],
+  ['abc', 'abc', 0],
+  ['', '', 0],
+  ['a', '', 1],
+  ['', 'a', 1],
+  ['abc', '', 3],
+  ['', 'abc', 3],
+  ['因為我是中國人所以我會說中文', '因為我是英國人所以我會說英文', 2]
+];
+
+var HELLO_WORDS = [
+  'BONJOUR',
+  'BINJOUR',
+  'BONTOUR',
+  'NONJOUR',
+  'ONJOUR',
+  'BONJOU',
+  'BONJOURE',
+  'BONTJOUR',
+  'TBONJOUR'
+];
+
+var HELLO_WORDS_TRANSPOSITIONS = [
+  'OBNJOUR',
+  'BNOJOUR',
+  'BOJNOUR',
+  'BONOJUR',
+  'BONJORU'
+];
+
+var LEVENSHTEIN_LTE1_TESTS = [
+  ['BONJOUR', 'BONJOURE', true],
+  ['TBONJOUR', 'BONJOUR', true],
+  ['BONTOUR', 'BONJOUR', true],
+  ['BONJOUR', 'MONJOUR', true],
+  ['BONJOUR', 'BONJOUT', true],
+  ['BMNJOURE', 'BONJOUR', false],
+  ['MONJOURE', 'BONJOUR', false],
+  ['BTONJOUR', 'BONJOUR', true],
+  ['BONJOUR', 'F', false],
+  ['BONJOUR', 'BNOJOURE', false],
+  ['BONJOUR', 'BONJOURES', false],
+  ['Faylan', 'Fayray', false],
+  ['Loane', 'Loona', false],
+  ['David Byrne', 'David Byron', false],
+  ['Willie Nix', 'Willie Nile', false],
+  ['DJ Spinna', 'DJ Spinbad', false],
+  ['Lian Ross', 'Diana Ross', false],
+  ['Diana', 'Lian', false]
+];
+
+describe('levenshtein-lte1', function() {
+  it('should work for trivial cases.', function() {
+    assert.strictEqual(levenshteinLte1('BONJOUR', 'BONJOUR'), 0);
+    assert.strictEqual(levenshteinLte1('HELLO', 'THISISVERYLONG'), Infinity);
+  });
+
+  it('should work with any kind of operations.', function() {
+    HELLO_WORDS.forEach(function(word) {
+      assert.strictEqual(levenshteinLte1(word, 'BONJOUR') <= 1, true, word);
+    });
+  });
+
+  it('should handle generic cases.', function() {
+    BASIC_TESTS.forEach(function(test) {
+      assert.strictEqual(
+        levenshteinLte1(test[0], test[1]),
+        test[2] <= 1 ? test[2] : Infinity,
+        test
+      );
+    });
+  });
+
+  it('should handle typical cases.', function() {
+    LEVENSHTEIN_LTE1_TESTS.forEach(function(test) {
+      assert.strictEqual(
+        levenshteinLte1(test[0], test[1]) <= 1,
+        test[2]
+      );
+    });
+  });
+});
