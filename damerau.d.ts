febcf230c3b209052fe3917fd718dd6628de57f5
Yomguithereal <guillaumeplique@gmail.com>
2020-01-07 17:15:38 +0100

    Fixing type declarations

--- damerau.d.ts
+++ damerau.d.ts
@@ -1,1 +1,4 @@
-export default function damerauLevenshteinLte1(A: string|Array, B: string|Array): number;
+declare function damerauLevenshteinLte1(A: string, B: string): number;
+declare function damerauLevenshteinLte1<T>(A: Array<T>, B: Array<T>): number;
+
+export default damerauLevenshteinLte1;
--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,4 @@
-export default function levenshteinLte1(A: string|Array, B: string|Array): number;
+declare function levenshteinLte1(A: string, B: string): number;
+declare function levenshteinLte1<T>(A: Array<T>, B: Array<T>): number;
+
+export default levenshteinLte1;
